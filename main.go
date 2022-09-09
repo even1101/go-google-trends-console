dddb3f361bd554686a4129b5021b344d1b30917e
even.chang <even1101u@gmail.com>
2022-09-09 15:02:16 +0800

    get trends and show

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const (
+	GoogleTrendsUrl = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=TW"
 )
 
 type RSS struct {
@@ -29,5 +36,45 @@ type News struct {
 }
 
 func main() {
-	fmt.Println("define struct")
+	var r RSS
+
+	data := readGoogleTrends()
+	err := xml.Unmarshal(data, &r)
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println("\nHere are the Google search trends for today")
+	fmt.Println("-------------------------------------------")
+
+	for i := range r.Channel.ItemList {
+		rank := (i + 1)
+		fmt.Println("#", rank)
+		fmt.Println("Search Term:", r.Channel.ItemList[i].Title)
+		fmt.Println("Link to trend:", r.Channel.ItemList[i].Link)
+		fmt.Println("Headline:", r.Channel.ItemList[i].NewsItems[0].Headline)
+		fmt.Println("Link to article:", r.Channel.ItemList[i].NewsItems[0].HeadlineLink)
+		fmt.Println("--------------------------")
+	}
+
+}
+
+func getGoogleTrends() *http.Response {
+	resp, err := http.Get(GoogleTrendsUrl)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return resp
+}
+
+func readGoogleTrends() []byte {
+	resp := getGoogleTrends()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return data
 }
